4930ad60b7f17a6c03cbbed32a8413ec96214865
lodosswkor <lodossw@gmail.com>
2023-01-07 11:35:54 +0900

    중간정리

---
+++ daanguen-mvc/src/main/java/repository/MysqlConnection.java
@@ -0,0 +1,31 @@
+package repository;
+
+/*
+ * 데이터베이스 연결 / 닫기 프로세스만 처리 
+ * */
+
+import java.sql.*;
+
+public class MysqlConnection {
+
+	private Connection conn = null;
+	
+	
+	public Connection getConnection() throws Exception {
+		String jdbcUrl = "jdbc:mysql://ideatags.io:3306/daanguen_mvc?user=root&password=";
+		return getConnection(jdbcUrl); 
+	} 
+	
+	public Connection getConnection(String jdbcUrl) throws Exception {
+		Class.forName("com.mysql.cj.jdbc.Driver");
+		conn = DriverManager.getConnection(jdbcUrl);
+		return conn; 
+	}
+	
+	
+	public void closeConnection() {
+		
+	}
+	
+	
+}
